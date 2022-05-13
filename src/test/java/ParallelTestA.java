8100fd2557da424f7d85373dbd0043c6d55663af
nataliiaGrynda <nataliiagrynda@gmail.com>
2022-05-13 11:59:21 -0500

    testing Introduction

--- src/test/java/ParallelTestA.java
+++ src/test/java/ParallelTestA.java
@@ -7,14 +7,12 @@ public class ParallelTestA {
         System.out.println("This is method1");
         System.out.println(Thread.currentThread().getId() + "\n");
     }
-
     @Test()
     public void method2() throws InterruptedException {
         Thread.sleep(12000);
         System.out.println("This is method2");
         System.out.println(Thread.currentThread().getId() + "\n");
     }
-
     @Test()
     public void method3() throws InterruptedException {
         Thread.sleep(13000);
--- src/test/java/ParallelTestB.java
+++ src/test/java/ParallelTestB.java
@@ -7,14 +7,12 @@ public class ParallelTestB {
         System.out.println("This is method1 from class B");
         System.out.println(Thread.currentThread().getId() + "\n");
     }
-
     @Test()
     public void method2() throws InterruptedException {
         Thread.sleep(12000);
         System.out.println("This is method2 from class B");
         System.out.println(Thread.currentThread().getId() + "\n");
     }
-
     @Test()
     public void method3() throws InterruptedException {
         Thread.sleep(13000);
